134197301e9d3ba316d140e20768c6833c73f95c
Patrick Policarpio <patrickpolicarpio08@gmail.com>
2024-12-27 15:34:36 +0800

    add all files

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import vue from "@vitejs/plugin-vue";
+import path from "node:path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@@types": path.resolve(__dirname, "./src/types"),
+    },
+  },
+});
